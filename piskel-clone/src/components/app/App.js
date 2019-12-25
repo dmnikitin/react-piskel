9846d3a11d122bee72867027cef6319b92f0fd40
Dmitry <joydnby@gmail.com>
2019-12-25 20:51:34 +0300

    feat: add basic react setup

---
+++ piskel-clone/src/components/app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>PISKEL</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
